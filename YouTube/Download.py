54da69ba460ed8b5ff2012ebc15f73b25db8ea1f
Rajdeep Srivastava <58080400+CAPTAIN-ARROW@users.noreply.github.com>
2020-03-02 02:28:37 +0530

    Add files via upload

---
+++ YouTube/Download.py
@@ -0,0 +1,9 @@
+# it's all about practicing
+
+import webbrowser
+
+# Enter your URL of video here
+url = " "   #copy your URL in this section
+download = url[:12] + "ss" + url[12:]
+
+webbrowser.open(download)
